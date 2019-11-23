48a7eb699061a637df4521d3ca22ec1ba9908021
OrangeFlag <25643404+OrangeFlag@users.noreply.github.com>
2019-11-23 10:47:17 +0300

    fix: eureka error DataSourceAutoConfiguration

--- eureka-service/src/main/kotlin/org/dei/eureka/EurekaApplication.kt
+++ eureka-service/src/main/kotlin/org/dei/eureka/EurekaApplication.kt
@@ -1,10 +1,13 @@
 package org.dei.eureka
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration::class])
 @EnableEurekaServer
 class EurekaApplication
 
